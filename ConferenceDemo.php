723233a7835e64079c13d05b64728c370fd92ccf
Daniel Pereira <dapereira@bandwidth.com>
2018-09-20 16:26:04 -0400

    Initial commit conference sample app

---
+++ ConferenceDemo.php
@@ -0,0 +1,64 @@
+<?php
+
+//Require Bandwidth SDK
+require_once('php-bandwidth/source/Catapult.php');
+
+//Create credentials
+$cred = new Catapult\Credentials('BANDWIDTH_USER_ID', 'BANDWIDTH_API_TOKEN', 'BANDWIDTH_API_SECRET');
+
+//Create client object
+$client = new Catapult\Client($cred);
+
+//Create a conference object
+$conference = new Catapult\Conference(array(
+"from" => "+1#########",  //Your Bandwidth number
+));
+
+//Sleep to avoid rate limit errors
+sleep(1);
+
+//Create an outbound call
+$call1 = new Catapult\Call(array(
+  "from" => "+1##########", //your Bandwidth number
+  "to" => "+1##########", //Conference member number
+  "conferenceId" => $conference->id
+));
+
+sleep(1);  
+
+//Create another outbound call
+$call2 = new Catapult\Call(array(
+  "from" => "+1##########",
+  "to" => "+1##########",
+  "conferenceId" => $conference->id
+));
+
+sleep(1);
+
+//Create another outbound call
+$call3 = new Catapult\Call(array(
+  "from" => "+1##########",
+  "to" => "+1##########",
+  "conferenceId" => $conference->id
+));
+
+sleep(1);
+
+//Now add the calls to the conference as members
+$member = $conference->addMember(array(
+   "callId" => $call1->id
+));
+
+sleep(1);
+
+$member2 = $conference->addMember(array(
+   "callId" => $call2->id
+));
+
+sleep(1);
+
+$member3 = $conference->addMember(array(
+   "callId" => $call3->id
+));
+
+?>
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# voice-conferencing-php
-An example of basic conferencing in PHP
+<div align="center">
+
+# Simple Conference Call - Outbound
+
+<a href="http://dev.bandwidth.com"><img src="https://s3.amazonaws.com/bwdemos/BW_Voice.png"/></a>
+</div>
+
+This is a short example PHP script that shows how to establish a conference call with outbound calls.
+
+## Table of Contents
+
+* [Prereqs](#prereqs)
+* [How It Works](#how-it-works)
+* [Deploy](#deploy)
+
+## Prereqs
+
+* Get the bandwidth php sdk [here](https://dev.bandwidth.com/php.html)
+* PHP 5.3 or greater
+* Bandwidth phone number and api [credentials](https://dev.bandwidth.com/getStarted.php)
+
+## How It Works
+
+This script creates a bandwidth client with the given credentials, then it creates a conference and creates three outbound call which are added as members to the conference.  If you run this script, all three callers should be placed into the conference call and hear each other when speaking. 
+
+## Deploy
+
+Message stating you can deploy this app to heroku. Click the button below for more information.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
